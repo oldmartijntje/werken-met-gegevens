a62ae8c2c23fad915f0432b9ea8af507442e8afe
Martijn Van Houwelingen <oldmartijntje@gmail.com>
2021-09-08 09:27:10 +0200

    F1.2.02.L3

--- f1.2.02.L1/Feestlunch.py
+++ f1.2.02.L1/Feestlunch.py
@@ -2,5 +2,6 @@ croissantjes = 17
 eurocent = 39
 stokbroden = 2
 euro = 2.78
-eindebrag = croissantjes * eurocent /100 + stokbroden * euro
-print(eindebrag)
+kortingsbonnen = 3
+eindebrag = (croissantjes * eurocent /100 + stokbroden * euro) - (kortingsbonnen * 0.50)
+print("De feestlunch kost je bij de bakker " + str(eindebrag) + " euro voor de " + str(croissantjes) + " croissantjes en de " + str(stokbroden) + " stokbroden als de " + str(kortingsbonnen) + " kortingsbonnen nog geldig zijn") 
--- f1.2.02.L1/de Speelhal-dag.py
+++ f1.2.02.L1/de Speelhal-dag.py
@@ -3,4 +3,4 @@ VIP_VR_GameSeat = 0.37
 minuten = 45
 vrienden = 3
 loonkosten = (toegangsticket + VIP_VR_GameSeat * minuten) * vrienden
-print(loonkosten)
+print("Dit geweldige dagje-uit met " + str(vrienden) + " mensen in de Speelhal met " + str(minuten) + " minuten VR kost je maar " + str(loonkosten) + " euro")
