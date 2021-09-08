bb4eb45df8144daedbfd31753cb36108754e0a7c
Martijn Van Houwelingen <oldmartijntje@gmail.com>
2021-09-08 09:14:52 +0200

    yeet

---
+++ f1.2.02.L1/Feestlunch.py
@@ -0,0 +1,6 @@
+croissantjes = 17
+eurocent = 39
+stokbroden = 2
+euro = 2.78
+eindebrag = croissantjes * eurocent /100 + stokbroden * euro
+print(eindebrag)
---
+++ f1.2.02.L1/de Speelhal-dag.py
@@ -0,0 +1,6 @@
+toegangsticket = 7.45
+VIP_VR_GameSeat = 0.37
+minuten = 45
+vrienden = 3
+loonkosten = (toegangsticket + VIP_VR_GameSeat * minuten) * vrienden
+print(loonkosten)
